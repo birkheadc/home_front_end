ba78b4f85fa72c80971ef49e0f7d991fa3ac300a
Colby Birkhead <birkheadc@gmail.com>
2022-06-01 17:27:31 +0900

    create .env (should not be checked into git

--- src/api/getMainSections/mockData/badDataDuplicateIds.js
+++ src/api/getMainSections/mockData/badDataDuplicateIds.js
@@ -1,20 +1,20 @@
 const badDataDuplicateIds = [
     {
         id: '0000',
-        sectionTitle: "First Section",
-        sectionSubTitle: "The first of many",
+        title: "First Section",
+        subtitle: "The first of many",
         body: "body"
     },
     {
         id: '0000',
-        sectionTitle: "Second Section",
-        sectionSubTitle: "The next in line",
+        title: "Second Section",
+        subtitle: "The next in line",
         body: "body"
     },
     {
         id: '0002',
-        sectionTitle: "Third Section",
-        sectionSubTitle: "",
+        title: "Third Section",
+        subtitle: "",
         body: "body"
     },
 ]
